318b2fd5d7e98efe74ce7115214c0d1a90f07d8e
filipibc <filipibc@gmail.com>
2022-02-06 18:48:29 -0300

    Input Data Manually

---
+++ src/PopulateArrayWithTheSameValue/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PopulateArrayWithTheSameValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----  How to Populate or Instantiate Array with a Same Value");
+            Console.WriteLine("-------------------------------------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("----- Input data manually");
+            var simpleArray = InstantiateArrayManually();
+            PrintArrayValues(simpleArray);
+
+
+
+            Console.WriteLine();
+        }
+
+        private static void PrintArrayValues(Article[] array)
+        {
+            if (array is null)
+                return;
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static Article[] InstantiateArrayManually()
+        {
+            return new Article[]
+            {
+                new Article { Title = "How to Copy Array Elements to New Array in C#", LastUpdate = new DateTime(2022,01,31)},
+                new Article { Title = "Named Arguments and Optional Parameters in C#", LastUpdate = new DateTime(2022,02,04)}
+            };
+        }
+    }
+}
